348a553046e8c7f6113f57eb42fa35287051e687
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-10-12 15:02:21 -0500

    REFACTOR: Mejora validacion de estatus de actividad

--- functions/src/validations/ValidateActividades.js
+++ functions/src/validations/ValidateActividades.js
@@ -10,42 +10,54 @@ const validateActividades = (() => {
     
     const validateCreateActividad = (bodyCreateActividad) => {
         let resultValidate = schemaCreateActividad.validate(bodyCreateActividad);
-        if (resultValidate.error) {
-            return createContentError("Algun dato fue enviado de manera equivocada", (resultValidate.error));
-        }
+        if (resultValidate.error)
+            return createContentError("Algun dato fue enviado de manera equivocada", resultValidate.error);
 
         resultValidate = schemaDate.validate(bodyCreateActividad.fecha_creada_task);
-        if (resultValidate.error) {
-            return createContentError("La fecha de alta no tiene el formato correcto 0000-00-00T00:00:00.000z", (resultValidate.error));
-        }
+        if (resultValidate.error) return createContentError(
+            "La fecha de alta no tiene el formato correcto 0000-00-00T00:00:00.000z",
+            resultValidate.error
+        );
 
         resultValidate = schemaDate.validate(bodyCreateActividad.fecha_modificada_task);
-        if (resultValidate.error) {
-            return createContentError("La fecha de actualizacion no tiene el formato correcto 0000-00-00T00:00:00.000z", (resultValidate.error));
-        }
+        if (resultValidate.error) return createContentError(
+            "La fecha de actualizacion no tiene el formato correcto 0000-00-00T00:00:00.000z",
+            resultValidate.error
+        );
+
+        if (
+            bodyCreateActividad.estatus_task.trim().toUpperCase() !== 'PENDIENTE' &&
+            bodyCreateActividad.estatus_task.trim().toUpperCase() !== 'REALIZADA' &&
+            bodyCreateActividad.estatus_task.trim().toUpperCase() !== 'CANCELADA'
+        ) return createContentError('El estatus de la actividad no es valido');
 
         return createContentAssert("Datos validados");
     }
 
     const validateUpdateActividad = (bodyUpdateActividad) => {
         let resultValidate = schemaUpdateActividad.validate(bodyUpdateActividad);
-        if (resultValidate.error) {
-            return createContentError("Algun dato fue enviado de manera equivocada", (resultValidate.error));
-        }
+        if (resultValidate.error)
+            return createContentError("Algun dato fue enviado de manera equivocada", resultValidate.error);
 
         resultValidate = schemaDate.validate(bodyUpdateActividad.fecha_modificada_task);
-        if (resultValidate.error) {
-            return createContentError("La fecha de actualizacion no tiene el formato correcto 0000-00-00T00:00:00.000z", (resultValidate.error));
-        }
+        if (resultValidate.error) return createContentError(
+            "La fecha de actualizacion no tiene el formato correcto 0000-00-00T00:00:00.000z",
+            resultValidate.error
+        );
+
+        if (
+            bodyUpdateActividad.estatus_task.trim().toUpperCase() !== 'PENDIENTE' &&
+            bodyUpdateActividad.estatus_task.trim().toUpperCase() !== 'REALIZADA' &&
+            bodyUpdateActividad.estatus_task.trim().toUpperCase() !== 'CANCELADA'
+        ) return createContentError('El estatus de la actividad no es valido');
 
         return createContentAssert("Datos validados");
     }
 
     const validateUpdateActividadStatus = (bodyActivo) => {
         let resultValidate = schemaUpdateActividadStatus.validate(bodyActivo);
-        if (resultValidate.error) {
-            return createContentError("Algun dato fue enviado de manera equivocada", (resultValidate.error));
-        }
+        if (resultValidate.error)
+            return createContentError("Algun dato fue enviado de manera equivocada", resultValidate.error);
 
         return createContentAssert("Datos validados");
     }

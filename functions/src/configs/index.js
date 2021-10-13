feb47b0c9902133af180b9db8c6acde4db1f69c2
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-10-13 11:47:30 -0500

    FIX: Corrige error al obtener los valores de configuracion

--- functions/src/configs/index.js
+++ functions/src/configs/index.js
@@ -3,10 +3,10 @@ require("mandatoryenv").load();
 
 const token = (functions.config().token) ? functions.config().token.key : process.env.CEITSSAT_TOKEN;
 const origin1 = (functions.config().origin1) ? functions.config().origin1.key : process.env.CEITSSAT_ORIGIN1;
-const origin2 = (functions.config().origin1) ? functions.config().origin2.key : process.env.CEITSSAT_ORIGIN2;
-const origin3 = (functions.config().origin1) ? functions.config().origin3.key : process.env.CEITSSAT_ORIGIN3;
-const origin4 = (functions.config().origin1) ? functions.config().origin4.key : process.env.CEITSSAT_ORIGIN4;
-const origin5 = (functions.config().origin1) ? functions.config().origin5.key : process.env.CEITSSAT_ORIGIN5;
+const origin2 = (functions.config().origin2) ? functions.config().origin2.key : process.env.CEITSSAT_ORIGIN2;
+const origin3 = (functions.config().origin3) ? functions.config().origin3.key : process.env.CEITSSAT_ORIGIN3;
+const origin4 = (functions.config().origin4) ? functions.config().origin4.key : process.env.CEITSSAT_ORIGIN4;
+const origin5 = (functions.config().origin5) ? functions.config().origin5.key : process.env.CEITSSAT_ORIGIN5;
 const user = (functions.config().user) ? functions.config().user.key : process.env.CEITSSAT_USER;
 const password = (functions.config().password) ? functions.config().password.key : process.env.CEITSSAT_PASSWORD;
 

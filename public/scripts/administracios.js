8907689bbc935b5b1acd534862293b28febeb219
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-11-24 17:04:26 -0600

    ADD: Agrega pestaña para agregar actividades

--- public/scripts/administracios.js
+++ public/scripts/administracios.js
@@ -22,6 +22,8 @@ var appAdministracion = new Vue({
             loadingCount: 0,
             widthWindow: 0,
             firtsSession: sessionStorage.getItem('calendario_firts_session'),
+            shiftSelected: false,
+
             // Para maestro actividades
             masterTask: localStorage.getItem('calendario_master_task') ?
                 JSON.parse(localStorage.getItem('calendario_master_task')) :
@@ -39,11 +41,41 @@ var appAdministracion = new Vue({
                 JSON.parse(localStorage.getItem('calendario_task_id_master')) :
                 { data: [] },
             arrayYearTasks: [],
+            statusTask: 0, //0 = lista, 1 = creando, 2 = Editando,
             taskNew: {
                 idMaster: null,
-                year: null,
                 rangoFechas: true,
+                year: null,
+                meses: [],
+                dias: [],
+                area: null,
+                titulo: '',
+                observaciones: '',
+                publica: true,
             },
+            dias: [
+                { day: 1, select: false }, { day: 2, select: false }, { day: 3, select: false }, { day: 4, select: false }, 
+                { day: 5, select: false }, { day: 6, select: false }, { day: 7, select: false }, { day: 8, select: false }, 
+                { day: 9, select: false }, { day: 10, select: false }, { day: 11, select: false }, { day: 12, select: false }, 
+                { day: 13, select: false }, { day: 14, select: false }, { day: 15, select: false }, { day: 16, select: false }, 
+                { day: 17, select: false }, { day: 18, select: false }, { day: 19, select: false }, { day: 20, select: false }, 
+                { day: 21, select: false }, { day: 22, select: false }, { day: 23, select: false }, { day: 24, select: false }, 
+                { day: 25, select: false }, { day: 26, select: false },{ day: 27, select: false }, { day: 28, select: false }, 
+                { day: 29, select: false }, { day: 30, select: false },{ day: 31, select: false }
+            ],
+            meses: [
+                { mes: 1, mesLetra: 'Enero', select: false }, { mes: 2, mesLetra: 'Febrero', select: false }, { mes: 3, mesLetra: 'Marzo', select: false },
+                { mes: 4, mesLetra: 'Abril', select: false }, { mes: 5, mesLetra: 'Mayo', select: false }, { mes: 6, mesLetra: 'Junio', select: false },
+                { mes: 7, mesLetra: 'Julio', select: false }, { mes: 8, mesLetra: 'Agosto', select: false }, { mes: 9, mesLetra: 'Septiembre', select: false },
+                { mes: 10, mesLetra: 'Octubre', select: false }, { mes: 11, mesLetra: 'Noviembre', select: false }, { mes: 12, mesLetra: 'Diciembre', select: false },
+            ],
+            diaSelected: {}, 
+            mesSelected: {},
+
+            // Data para areas
+            listAreas: localStorage.getItem('calendario_areas_data') ?
+                JSON.parse(localStorage.getItem('calendario_areas_data')) :
+                { data: [] },
         }
     },
     computed: {
@@ -84,7 +116,7 @@ var appAdministracion = new Vue({
 
         // Para actividades
         iconTasksFloat() {
-            return this.showOptionsTasks ? 'icofont-close-circled' : 'icofont-sub-listing';
+            return this.showOptionsTasks ? 'icofont-close' : 'icofont-sub-listing';
         },
         styleFloatTasks() {
             return this.showOptionsTasks ? 'opacity: 1.0; right: 20pt;' : 'opacity: 0.0; right: 15pt;';
@@ -97,19 +129,37 @@ var appAdministracion = new Vue({
                 'Lista maestro: ' + this.listTaskByIdMaster.data[0].id_master_task.titulo :
                 'Lista vacia'
         },
+        titleCardArea() {
+            return this.statusTask === 1 ? 'Creando actividad' : 'Editando actividad'
+        },
+
+        // Para areas
+        areasRefactor() {
+            return this.listAreas.data
+        },
     },
     mounted() {
         this.widthWindow = window.innerWidth;
         if (!this.login) window.location.href = '../index.html';
         else {
-            console.log(this.firtsSession);
             if (this.firtsSession === 'SI') {
                 this.loadListTasks();
-                sessionStorage.setItem('calendario_firts_session', 'NO')
+                this.loadAreas();
+                this.loadTasksByIdMaster(this.idMasterTaskSearch);
+                sessionStorage.setItem('calendario_firts_session', 'NO');
             }
+            
+            this.loadAreas();
             const dateActual = this.getDateNow();
             const yearInitial = parseInt(dateActual.format('YYYY'));
             for (let index = 0; index < 5; index++) this.arrayYearTasks.push(yearInitial + index);
+            
+            window.addEventListener('keyup', (evt) => {
+                if (evt.key === 'Shift') this.shiftSelected = false;
+            });
+            window.addEventListener('keydown', (evt) => {
+                if (evt.key === 'Shift') this.shiftSelected = true;
+            });
         }
     },
     methods: {
@@ -353,6 +403,47 @@ var appAdministracion = new Vue({
         },
         
         // Para actividades
+        daySelected(dia) {
+            return dia.select ? 'bg-primary' : 'bg-light text-dark';
+        },
+        setSelectedDay(dia) {
+            const daysSelected = this.dias.filter((dia) => dia.select).length;
+            dia.select = !dia.select;
+            if (daysSelected === 0) this.diaSelected = dia;
+            else if (this.shiftSelected) {
+                this.dias.forEach((dia) => { dia.select = false });
+                if ((dia.day - this.diaSelected.day) < 0) {
+                    for (let index = this.diaSelected.day; index >= dia.day; index--) {
+                        const position = this.dias.find((dia) => dia.day === index)
+                        if (position) position.select = true;
+                    }
+                } else {
+                    for (let index = this.diaSelected.day; index <= dia.day; index++) {
+                        const position = this.dias.find((dia) => dia.day === index)
+                        if (position) position.select = true;
+                    }
+                }
+            }
+        },
+        setSelectedMonth(mes) {
+            const monthsSelected = this.meses.filter((mes) => mes.select).length;
+            mes.select = !mes.select;
+            if (monthsSelected === 0) this.mesSelected = mes;
+            else if (this.shiftSelected) {
+                this.meses.forEach((mes) => { mes.select = false });
+                if ((mes.mes - this.mesSelected.mes) < 0) {
+                    for (let index = this.mesSelected.mes; index >= mes.mes; index--) {
+                        const position = this.meses.find((mes) => mes.mes === index)
+                        if (position) position.select = true;
+                    }
+                } else {
+                    for (let index = this.mesSelected.mes; index <= mes.mes; index++) {
+                        const position = this.meses.find((mes) => mes.mes === index)
+                        if (position) position.select = true;
+                    }
+                }
+            }
+        },
         showOptionsTasksClick() { this.showOptionsTasks = !this.showOptionsTasks;},
         searchListTaskByIdMaster() {
             if(this.idMasterSelected === null)
@@ -362,6 +453,7 @@ var appAdministracion = new Vue({
         },
         reloadListTaskByIdMaster() {
             this.showOptionsTasks = false;
+            this.statusTask = 0;
             if (this.idMasterTaskSearch.trim() !== '')
                 this.loadTasksByIdMaster(this.idMasterTaskSearch);
         },
@@ -403,6 +495,43 @@ var appAdministracion = new Vue({
         },
         newTask() {
             this.showOptionsTasks = false;
+            this.statusTask = 1
+        },
+
+        // Methodos para areas
+        async loadAreas() {
+            try {
+                this.showOptionsTasks = false;
+                const url =
+                'https://us-central1-calendarioescolaritssat.cloudfunctions.net/api/v1/areas';
+
+                this.setLoading(true);
+
+                const response = await axios({
+                    method: 'get',
+                    url,
+                })
+                console.log(response);
+
+                this.setLoading(false);
+
+                if (response.data.success) {
+                    localStorage.setItem(
+                        'calendario_areas_data',
+                        JSON.stringify(response.data)
+                    )
+                    this.listAreas = response.data;
+                } else {
+                    this.showAlert(response.data.message, 'Fallo al cargar las areas', 'warning')
+                }
+            } catch (error) {
+                console.log(error, error.response);
+                this.setLoading(false);
+                if (error.response !== undefined)
+                    this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
+                else
+                    this.showAlert('Fallo cargar areas intentelo mas tarde', 'Error inesperado', 'danger');
+            }
         },
     },
 })

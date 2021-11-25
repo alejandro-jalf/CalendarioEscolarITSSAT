59c42218b68d9f375694bef6ad7e27e8a6dc8f03
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-11-25 13:21:31 -0600

    REFACTOR: Cambia estructura de pestañas

---
+++ public/scripts/actividades.js
@@ -0,0 +1,341 @@
+if (!sessionStorage.getItem('calendario_firts_session'))
+    sessionStorage.setItem('calendario_firts_session', 'SI')
+if (!localStorage.getItem('calendario_id_master_selected'))
+    localStorage.setItem('calendario_id_master_selected', '')
+
+var appAdministracion = new Vue({
+    el: '#app',
+    data() {
+        return {
+            login:  (typeof localStorage.getItem('calendario_p_login') === 'string') ?
+                localStorage.getItem('calendario_p_login') === 'true' :
+                localStorage.getItem('calendario_p_login'),
+            dataUser: localStorage.getItem('calendario_data_user') ?
+                JSON.parse(localStorage.getItem('calendario_data_user')) :
+                { data: {}, empty: true },
+            alert: {
+                title: 'Advertencia',
+                message: 'Any massage',
+                type: 'warning',
+                component: null,
+            },
+            loadingCount: 0,
+            widthWindow: 0,
+            firtsSession: sessionStorage.getItem('calendario_firts_session'),
+            shiftSelected: false,
+
+            masterTask: localStorage.getItem('calendario_master_task') ?
+                JSON.parse(localStorage.getItem('calendario_master_task')) :
+                { data: [] },
+            idMasterSelected: null,
+            idMasterTaskSearch: localStorage.getItem('calendario_id_master_selected'),
+            showOptionsTasks: false,
+            listTaskByIdMaster: localStorage.getItem('calendario_task_id_master') ?
+                JSON.parse(localStorage.getItem('calendario_task_id_master')) :
+                { data: [] },
+            arrayYearTasks: [],
+            statusTask: 0, //0 = lista, 1 = creando, 2 = Editando,
+            taskNew: {
+                idMaster: null,
+                rangoFechas: true,
+                year: null,
+                meses: [],
+                dias: [],
+                area: null,
+                titulo: '',
+                observaciones: '',
+                publica: true,
+            },
+            dias: [
+                { day: 1, select: false }, { day: 2, select: false }, { day: 3, select: false }, { day: 4, select: false }, 
+                { day: 5, select: false }, { day: 6, select: false }, { day: 7, select: false }, { day: 8, select: false }, 
+                { day: 9, select: false }, { day: 10, select: false }, { day: 11, select: false }, { day: 12, select: false }, 
+                { day: 13, select: false }, { day: 14, select: false }, { day: 15, select: false }, { day: 16, select: false }, 
+                { day: 17, select: false }, { day: 18, select: false }, { day: 19, select: false }, { day: 20, select: false }, 
+                { day: 21, select: false }, { day: 22, select: false }, { day: 23, select: false }, { day: 24, select: false }, 
+                { day: 25, select: false }, { day: 26, select: false },{ day: 27, select: false }, { day: 28, select: false }, 
+                { day: 29, select: false }, { day: 30, select: false },{ day: 31, select: false }
+            ],
+            meses: [
+                { mes: 1, mesLetra: 'Enero', select: false }, { mes: 2, mesLetra: 'Febrero', select: false }, { mes: 3, mesLetra: 'Marzo', select: false },
+                { mes: 4, mesLetra: 'Abril', select: false }, { mes: 5, mesLetra: 'Mayo', select: false }, { mes: 6, mesLetra: 'Junio', select: false },
+                { mes: 7, mesLetra: 'Julio', select: false }, { mes: 8, mesLetra: 'Agosto', select: false }, { mes: 9, mesLetra: 'Septiembre', select: false },
+                { mes: 10, mesLetra: 'Octubre', select: false }, { mes: 11, mesLetra: 'Noviembre', select: false }, { mes: 12, mesLetra: 'Diciembre', select: false },
+            ],
+            diaSelected: {}, 
+            mesSelected: {},
+
+            // Master
+            async loadListTasks() {
+                try {
+                    this.showOptionsMaster = false;
+                    const url =
+                    'https://us-central1-calendarioescolaritssat.cloudfunctions.net/api/v1/maestroactividades';
+
+                    this.setLoading(true);
+
+                    const response = await axios({
+                        method: 'get',
+                        url,
+                        
+                    })
+
+                    this.setLoading(false);
+
+                    if (response.data.success) {
+                        localStorage.setItem(
+                            'calendario_master_task',
+                            JSON.stringify(response.data)
+                        )
+                        this.masterTask = response.data;
+                    } else {
+                        this.showAlert(response.data.message, 'Fallo al cargar las listas de actividades', 'warning')
+                    }
+                } catch (error) {
+                    console.log(error, error.response);
+                    this.setLoading(false);
+                    if (error.response !== undefined)
+                        this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
+                    else
+                        this.showAlert('Fallo cargar las listas de actividades intentelo mas tarde', 'Error inesperado', 'danger');
+                }
+            },
+
+            // Data para areas
+            listAreas: localStorage.getItem('calendario_areas_data') ?
+                JSON.parse(localStorage.getItem('calendario_areas_data')) :
+                { data: [] },
+        }
+    },
+    computed: {
+        loading() {
+            return this.loadingCount > 0
+        },
+        apodo() {
+            return this.dataUser.data[0].nombre_user
+        },
+        backgroundHeader() {
+            if (this.alert.type === 'warning') return 'bg-warning';
+            if (this.alert.type === 'info') return 'bg-info';
+            if (this.alert.type === 'success') return 'bg-success';
+            if (this.alert.type === 'primary') return 'bg-primary';
+            if (this.alert.type === 'danger') return 'bg-danger';
+            if (this.alert.type === 'dark') return 'bg-dark';
+            return 'bg-warning';
+        },
+        // Para actividades
+        iconTasksFloat() {
+            return this.showOptionsTasks ? 'icofont-close' : 'icofont-sub-listing';
+        },
+        styleFloatTasks() {
+            return this.showOptionsTasks ? 'opacity: 1.0; right: 20pt;' : 'opacity: 0.0; right: 15pt;';
+        },
+        listTaskByIdMasterRefactor() {
+            return this.listTaskByIdMaster.data;
+        },
+        nameMasterTask() {
+            return (this.listTaskByIdMaster.data.length > 0) ?
+                'Lista maestro: ' + this.listTaskByIdMaster.data[0].id_master_task.titulo :
+                'Lista vacia'
+        },
+        titleCardArea() {
+            return this.statusTask === 1 ? 'Creando actividad' : 'Editando actividad'
+        },
+        masterTaskRefactor() {
+            return this.masterTask.data.sort((a, b) => {
+                return new moment(a.fecha_creada_master_task.replace('z', '')) <= new moment(b.fecha_creada_master_task.replace('z', '')) ? 1 : -1;
+            });
+        },
+
+        // Para areas
+        areasRefactor() {
+            return this.listAreas.data
+        },
+    },
+    mounted() {
+        this.widthWindow = window.innerWidth;
+        if (!this.login) window.location.href = '../index.html';
+        else {
+            if (this.firtsSession === 'SI') {
+                this.loadAreas();
+                this.loadTasksByIdMaster(this.idMasterTaskSearch);
+                sessionStorage.setItem('calendario_firts_session', 'NO');
+            }
+            
+            const dateActual = this.getDateNow();
+            const yearInitial = parseInt(dateActual.format('YYYY'));
+            for (let index = 0; index < 5; index++) this.arrayYearTasks.push(yearInitial + index);
+            
+            window.addEventListener('keyup', (evt) => {
+                if (evt.key === 'Shift') this.shiftSelected = false;
+            });
+            window.addEventListener('keydown', (evt) => {
+                if (evt.key === 'Shift') this.shiftSelected = true;
+            });
+        }
+    },
+    methods: {
+        formatDate(dateString, hours = false) {
+            const formatHours = hours ? ' HH:MM:ss' : '';
+            const dateFromat = dateString.replace('z', '');
+            const datef = new moment(dateFromat);
+            const formatApply = datef.format(`DD/MM/YYYY ${formatHours}`);
+            return formatApply;
+        },
+        closeSession() {
+            this.login = false;
+            localStorage.setItem('calendario_p_login', false);
+            window.location.href = '../index.html';
+        },
+        getDateNow() {
+            return new moment().local(true)
+        },
+        setLoading(visible = false) {
+            if (visible) this.loadingCount ++;
+            else this.loadingCount --;
+            if (this.loadingCount < 0) this.loadingCount = 0;
+        },
+        showAlert(message, title = 'Advertencia', type = 'warning') {
+            this.alert.title = title;
+            this.alert.message = message;
+            this.alert.type = type;
+
+            this.$refs.btnAlert.click()
+        },
+
+        // Para actividades
+        daySelected(dia) {
+            return dia.select ? 'bg-primary' : 'bg-light text-dark';
+        },
+        setSelectedDay(dia) {
+            const daysSelected = this.dias.filter((dia) => dia.select).length;
+            dia.select = !dia.select;
+            if (daysSelected === 0) this.diaSelected = dia;
+            else if (this.shiftSelected) {
+                this.dias.forEach((dia) => { dia.select = false });
+                if ((dia.day - this.diaSelected.day) < 0) {
+                    for (let index = this.diaSelected.day; index >= dia.day; index--) {
+                        const position = this.dias.find((dia) => dia.day === index)
+                        if (position) position.select = true;
+                    }
+                } else {
+                    for (let index = this.diaSelected.day; index <= dia.day; index++) {
+                        const position = this.dias.find((dia) => dia.day === index)
+                        if (position) position.select = true;
+                    }
+                }
+            }
+        },
+        setSelectedMonth(mes) {
+            const monthsSelected = this.meses.filter((mes) => mes.select).length;
+            mes.select = !mes.select;
+            if (monthsSelected === 0) this.mesSelected = mes;
+            else if (this.shiftSelected) {
+                this.meses.forEach((mes) => { mes.select = false });
+                if ((mes.mes - this.mesSelected.mes) < 0) {
+                    for (let index = this.mesSelected.mes; index >= mes.mes; index--) {
+                        const position = this.meses.find((mes) => mes.mes === index)
+                        if (position) position.select = true;
+                    }
+                } else {
+                    for (let index = this.mesSelected.mes; index <= mes.mes; index++) {
+                        const position = this.meses.find((mes) => mes.mes === index)
+                        if (position) position.select = true;
+                    }
+                }
+            }
+        },
+        showOptionsTasksClick() { this.showOptionsTasks = !this.showOptionsTasks;},
+        searchListTaskByIdMaster() {
+            if(this.idMasterSelected === null)
+                this.showAlert('Necesita seleccionar una lista de actividades');
+            else
+                this.loadTasksByIdMaster(this.idMasterSelected)
+        },
+        reloadListTaskByIdMaster() {
+            this.loadListTasks();
+            this.showOptionsTasks = false;
+            this.statusTask = 0;
+            if (this.idMasterTaskSearch.trim() !== '')
+                this.loadTasksByIdMaster(this.idMasterTaskSearch);
+        },
+        async loadTasksByIdMaster(idMasterSelected) {
+            try {
+                this.showOptionsTasks = false;
+                const url =
+                'https://us-central1-calendarioescolaritssat.cloudfunctions.net/api/v1/actividades/maestroactividades/' +
+                idMasterSelected;
+
+                this.setLoading(true);
+
+                const response = await axios({
+                    method: 'get',
+                    url,
+                    
+                })
+
+                this.setLoading(false);
+
+                if (response.data.success) {
+                    localStorage.setItem(
+                        'calendario_task_id_master',
+                        JSON.stringify(response.data)
+                    )
+                    this.listTaskByIdMaster = response.data;
+                    this.idMasterTaskSearch = idMasterSelected;
+                    localStorage.setItem('calendario_id_master_selected', idMasterSelected);
+                } else {
+                    this.showAlert(response.data.message, 'Fallo al cargar las actividades', 'warning')
+                }
+            } catch (error) {
+                console.log(error, error.response);
+                this.setLoading(false);
+                if (error.response !== undefined)
+                    this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
+                else
+                    this.showAlert('Fallo cargar actividades intentelo mas tarde', 'Error inesperado', 'danger');
+            }
+        },
+        newTask() {
+            this.showOptionsTasks = false;
+            this.statusTask = 1
+        },
+
+        // Methodos para areas
+        async loadAreas() {
+            try {
+                this.showOptionsTasks = false;
+                const url =
+                'https://us-central1-calendarioescolaritssat.cloudfunctions.net/api/v1/areas';
+
+                this.setLoading(true);
+
+                const response = await axios({
+                    method: 'get',
+                    url,
+                    
+                })
+                console.log(response);
+
+                this.setLoading(false);
+
+                if (response.data.success) {
+                    localStorage.setItem(
+                        'calendario_areas_data',
+                        JSON.stringify(response.data)
+                    )
+                    this.listAreas = response.data;
+                } else {
+                    this.showAlert(response.data.message, 'Fallo al cargar las areas', 'warning')
+                }
+            } catch (error) {
+                console.log(error, error.response);
+                this.setLoading(false);
+                if (error.response !== undefined)
+                    this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
+                else
+                    this.showAlert('Fallo cargar areas intentelo mas tarde', 'Error inesperado', 'danger');
+            }
+        },
+    },
+})
--- public/scripts/maestroactividades.js
+++ public/scripts/maestroactividades.js
@@ -23,8 +23,8 @@ var appAdministracion = new Vue({
             widthWindow: 0,
             firtsSession: sessionStorage.getItem('calendario_firts_session'),
             shiftSelected: false,
+            arrayYearTasks: [],
 
-            // Para maestro actividades
             masterTask: localStorage.getItem('calendario_master_task') ?
                 JSON.parse(localStorage.getItem('calendario_master_task')) :
                 { data: [] },
@@ -32,50 +32,6 @@ var appAdministracion = new Vue({
             statusMasterTask: 0,
             masterTaskActual: {},
             masterTaskActualEdit: {},
-
-            // Para actividades
-            idMasterSelected: null,
-            idMasterTaskSearch: localStorage.getItem('calendario_id_master_selected'),
-            showOptionsTasks: false,
-            listTaskByIdMaster: localStorage.getItem('calendario_task_id_master') ?
-                JSON.parse(localStorage.getItem('calendario_task_id_master')) :
-                { data: [] },
-            arrayYearTasks: [],
-            statusTask: 0, //0 = lista, 1 = creando, 2 = Editando,
-            taskNew: {
-                idMaster: null,
-                rangoFechas: true,
-                year: null,
-                meses: [],
-                dias: [],
-                area: null,
-                titulo: '',
-                observaciones: '',
-                publica: true,
-            },
-            dias: [
-                { day: 1, select: false }, { day: 2, select: false }, { day: 3, select: false }, { day: 4, select: false }, 
-                { day: 5, select: false }, { day: 6, select: false }, { day: 7, select: false }, { day: 8, select: false }, 
-                { day: 9, select: false }, { day: 10, select: false }, { day: 11, select: false }, { day: 12, select: false }, 
-                { day: 13, select: false }, { day: 14, select: false }, { day: 15, select: false }, { day: 16, select: false }, 
-                { day: 17, select: false }, { day: 18, select: false }, { day: 19, select: false }, { day: 20, select: false }, 
-                { day: 21, select: false }, { day: 22, select: false }, { day: 23, select: false }, { day: 24, select: false }, 
-                { day: 25, select: false }, { day: 26, select: false },{ day: 27, select: false }, { day: 28, select: false }, 
-                { day: 29, select: false }, { day: 30, select: false },{ day: 31, select: false }
-            ],
-            meses: [
-                { mes: 1, mesLetra: 'Enero', select: false }, { mes: 2, mesLetra: 'Febrero', select: false }, { mes: 3, mesLetra: 'Marzo', select: false },
-                { mes: 4, mesLetra: 'Abril', select: false }, { mes: 5, mesLetra: 'Mayo', select: false }, { mes: 6, mesLetra: 'Junio', select: false },
-                { mes: 7, mesLetra: 'Julio', select: false }, { mes: 8, mesLetra: 'Agosto', select: false }, { mes: 9, mesLetra: 'Septiembre', select: false },
-                { mes: 10, mesLetra: 'Octubre', select: false }, { mes: 11, mesLetra: 'Noviembre', select: false }, { mes: 12, mesLetra: 'Diciembre', select: false },
-            ],
-            diaSelected: {}, 
-            mesSelected: {},
-
-            // Data para areas
-            listAreas: localStorage.getItem('calendario_areas_data') ?
-                JSON.parse(localStorage.getItem('calendario_areas_data')) :
-                { data: [] },
         }
     },
     computed: {
@@ -95,7 +51,6 @@ var appAdministracion = new Vue({
             return 'bg-warning';
         },
 
-        // Para maestro actividades
         styleFloatMaster() {
             return this.showOptionsMaster ? 'opacity: 1.0; right: 20pt;' : 'opacity: 0.0; right: 15pt;';
         },
@@ -113,30 +68,6 @@ var appAdministracion = new Vue({
                 return new moment(a.fecha_creada_master_task.replace('z', '')) <= new moment(b.fecha_creada_master_task.replace('z', '')) ? 1 : -1;
             });
         },
-
-        // Para actividades
-        iconTasksFloat() {
-            return this.showOptionsTasks ? 'icofont-close' : 'icofont-sub-listing';
-        },
-        styleFloatTasks() {
-            return this.showOptionsTasks ? 'opacity: 1.0; right: 20pt;' : 'opacity: 0.0; right: 15pt;';
-        },
-        listTaskByIdMasterRefactor() {
-            return this.listTaskByIdMaster.data;
-        },
-        nameMasterTask() {
-            return (this.listTaskByIdMaster.data.length > 0) ?
-                'Lista maestro: ' + this.listTaskByIdMaster.data[0].id_master_task.titulo :
-                'Lista vacia'
-        },
-        titleCardArea() {
-            return this.statusTask === 1 ? 'Creando actividad' : 'Editando actividad'
-        },
-
-        // Para areas
-        areasRefactor() {
-            return this.listAreas.data
-        },
     },
     mounted() {
         this.widthWindow = window.innerWidth;
@@ -144,12 +75,9 @@ var appAdministracion = new Vue({
         else {
             if (this.firtsSession === 'SI') {
                 this.loadListTasks();
-                this.loadAreas();
-                this.loadTasksByIdMaster(this.idMasterTaskSearch);
                 sessionStorage.setItem('calendario_firts_session', 'NO');
             }
-            
-            this.loadAreas();
+
             const dateActual = this.getDateNow();
             const yearInitial = parseInt(dateActual.format('YYYY'));
             for (let index = 0; index < 5; index++) this.arrayYearTasks.push(yearInitial + index);
@@ -191,7 +119,6 @@ var appAdministracion = new Vue({
             this.$refs.btnAlert.click()
         },
 
-        // Para maestro actividades
         showOptionsMasterClick() { this.showOptionsMaster = !this.showOptionsMaster; },
         listMasterTask() {
             this.statusMasterTask = 0;
@@ -399,138 +326,6 @@ var appAdministracion = new Vue({
                     this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
                 else
                     this.showAlert('Fallo al eliminar maestro actividad intentelo mas tarde', 'Error inesperado', 'danger');
-            }
-        },
-        
-        // Para actividades
-        daySelected(dia) {
-            return dia.select ? 'bg-primary' : 'bg-light text-dark';
-        },
-        setSelectedDay(dia) {
-            const daysSelected = this.dias.filter((dia) => dia.select).length;
-            dia.select = !dia.select;
-            if (daysSelected === 0) this.diaSelected = dia;
-            else if (this.shiftSelected) {
-                this.dias.forEach((dia) => { dia.select = false });
-                if ((dia.day - this.diaSelected.day) < 0) {
-                    for (let index = this.diaSelected.day; index >= dia.day; index--) {
-                        const position = this.dias.find((dia) => dia.day === index)
-                        if (position) position.select = true;
-                    }
-                } else {
-                    for (let index = this.diaSelected.day; index <= dia.day; index++) {
-                        const position = this.dias.find((dia) => dia.day === index)
-                        if (position) position.select = true;
-                    }
-                }
-            }
-        },
-        setSelectedMonth(mes) {
-            const monthsSelected = this.meses.filter((mes) => mes.select).length;
-            mes.select = !mes.select;
-            if (monthsSelected === 0) this.mesSelected = mes;
-            else if (this.shiftSelected) {
-                this.meses.forEach((mes) => { mes.select = false });
-                if ((mes.mes - this.mesSelected.mes) < 0) {
-                    for (let index = this.mesSelected.mes; index >= mes.mes; index--) {
-                        const position = this.meses.find((mes) => mes.mes === index)
-                        if (position) position.select = true;
-                    }
-                } else {
-                    for (let index = this.mesSelected.mes; index <= mes.mes; index++) {
-                        const position = this.meses.find((mes) => mes.mes === index)
-                        if (position) position.select = true;
-                    }
-                }
-            }
-        },
-        showOptionsTasksClick() { this.showOptionsTasks = !this.showOptionsTasks;},
-        searchListTaskByIdMaster() {
-            if(this.idMasterSelected === null)
-                this.showAlert('Necesita seleccionar una lista de actividades');
-            else
-                this.loadTasksByIdMaster(this.idMasterSelected)
-        },
-        reloadListTaskByIdMaster() {
-            this.showOptionsTasks = false;
-            this.statusTask = 0;
-            if (this.idMasterTaskSearch.trim() !== '')
-                this.loadTasksByIdMaster(this.idMasterTaskSearch);
-        },
-        async loadTasksByIdMaster(idMasterSelected) {
-            try {
-                this.showOptionsTasks = false;
-                const url =
-                'https://us-central1-calendarioescolaritssat.cloudfunctions.net/api/v1/actividades/maestroactividades/' +
-                idMasterSelected;
-
-                this.setLoading(true);
-
-                const response = await axios({
-                    method: 'get',
-                    url,
-                })
-
-                this.setLoading(false);
-
-                if (response.data.success) {
-                    localStorage.setItem(
-                        'calendario_task_id_master',
-                        JSON.stringify(response.data)
-                    )
-                    this.listTaskByIdMaster = response.data;
-                    this.idMasterTaskSearch = idMasterSelected;
-                    localStorage.setItem('calendario_id_master_selected', idMasterSelected);
-                } else {
-                    this.showAlert(response.data.message, 'Fallo al cargar las actividades', 'warning')
-                }
-            } catch (error) {
-                console.log(error, error.response);
-                this.setLoading(false);
-                if (error.response !== undefined)
-                    this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
-                else
-                    this.showAlert('Fallo cargar actividades intentelo mas tarde', 'Error inesperado', 'danger');
-            }
-        },
-        newTask() {
-            this.showOptionsTasks = false;
-            this.statusTask = 1
-        },
-
-        // Methodos para areas
-        async loadAreas() {
-            try {
-                this.showOptionsTasks = false;
-                const url =
-                'https://us-central1-calendarioescolaritssat.cloudfunctions.net/api/v1/areas';
-
-                this.setLoading(true);
-
-                const response = await axios({
-                    method: 'get',
-                    url,
-                })
-                console.log(response);
-
-                this.setLoading(false);
-
-                if (response.data.success) {
-                    localStorage.setItem(
-                        'calendario_areas_data',
-                        JSON.stringify(response.data)
-                    )
-                    this.listAreas = response.data;
-                } else {
-                    this.showAlert(response.data.message, 'Fallo al cargar las areas', 'warning')
-                }
-            } catch (error) {
-                console.log(error, error.response);
-                this.setLoading(false);
-                if (error.response !== undefined)
-                    this.showAlert(error.response.data.message, 'Error inesperado', 'danger');
-                else
-                    this.showAlert('Fallo cargar areas intentelo mas tarde', 'Error inesperado', 'danger');
             }
         },
     },
